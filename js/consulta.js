9aff5d5faba56afd10a1b13f8f7d2a8252d1e602
josema <josemufarrege@gmail.com>
2021-08-21 09:31:04 -0300

    Simulador de Cuota mensual

--- js/consulta.js
+++ js/consulta.js
@@ -5,3 +5,16 @@ document.getElementById("send").addEventListener("click",function(){
         alert("El campo mensaje no debe estar vacio");
     }
 })
+
+document.getElementById("calculate").addEventListener("click",function(){
+    let quote = parseInt(document.getElementById("quote").value)
+    
+    if (quote >= 150){
+        let countFamily = Math.round(quote / 150)
+        document.getElementById('resultado').innerHTML='Estaria ayudando a '+ countFamily +' familias'
+    }else{
+        alert("Debe ingresar un monto mayor a $150")
+        document.getElementById('resultado').innerHTML = ""
+    }
+    
+})
