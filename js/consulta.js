1d9ab766a16540c6bf8656521b16cfad6e9955f1
josema <josemufarrege@gmail.com>
2021-09-16 18:09:47 -0300

    Se reemplaza eventos y referencias usando Jquery

--- js/consulta.js
+++ js/consulta.js
@@ -1,42 +1,43 @@
-document.getElementById("send").addEventListener("click",function(){
-
-    //Se valida si el campo mensaje esta vacio
-    if (document.getElementById("firstName").value == ""){
+$("#send").click(function(){
+    //Se valida si el campo nombre esta vacio
+    if ($("#firstName").val() == ""){
         alert("El campo Nombre no debe estar vacio");
         return;
     }
-
-    if (document.getElementById("lastName").value == ""){
+    //Se valida si el campo apellido esta vacio
+    if ($("#lastname").val() == ""){
         alert("El campo Apellido no debe estar vacio");
         return;
     }
-
-    if (document.getElementById("mail").value == ""){
+    //Se valida si el campo mail esta vacio
+    if ($("#mail").val() == ""){
         alert("El campo Email no debe estar vacio");
         return;
     }else{
-        let mail = document.getElementById("mail").value;
+        //Se valida si el mail tiene formato correcto
+        let mail = $("#mail").val();
         if (validarEmail(mail) == false){
             alert("El mail ingresado es erroneo");
             return;
         }
     }
-
-    if (document.getElementById("message").value == ""){
+    //Se valida si el campo mensaje esta vacio
+    if ($("#message").val() == ""){
         alert("El campo Mensaje no debe estar vacio");        
   
     }
 })
 
-document.getElementById("calculate").addEventListener("click",function(){
-    let quote = parseInt(document.getElementById("quote").value)
+$("#calculate").click(function(){
+
+    let quote = parseInt($("#quote").val());
     
     if (quote >= 150){
         let countFamily = Math.round(quote / 150)
-        document.getElementById('resultado').innerHTML='Estaria ayudando a '+ countFamily +' familias'
+        $('#resultado').html('Estaria ayudando a '+ countFamily +' familias');
     }else{
         alert("Debe ingresar un monto mayor a $150")
-        document.getElementById('resultado').innerHTML = ""
+        $('#resultado').html("");
     }
     
 })
